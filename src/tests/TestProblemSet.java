1385aeeddfb8d1b52999c79790dc6db627ada850
Devan <dng8597@bths.edu>
2023-09-12 14:02:54 -0400

    finished up tests

--- src/tests/TestProblemSet.java
+++ src/tests/TestProblemSet.java
@@ -8,46 +8,67 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
+
 
 public class TestProblemSet {
 
     // TODO: Write testing for all 3 methods of the ratings.ProblemSet class
 
-        @Test
-    public void StringTest() {
+    @Test
+    public void AverageTest() {
 
-        ArrayList<Double> list = new ArrayList<>() {{add(2.5); add(2.5); add(2.5); add(2.5);}};
+        ArrayList<Double> list = new ArrayList<>() {{
+            add(2.5);
+            add(2.5);
+            add(2.5);
+            add(2.5);
+        }};
         assertTrue(ProblemSet.average(list) == 2.5);
-        ArrayList<Double> list_dx = new ArrayList<>() {{add(2.5); add(2.5); add(2.5); add(2.5);}};
+        ArrayList<Double> list_dx = new ArrayList<>() {{
+            add(6.5);
+            add(6.5);
+            add(8.5);
+            add(8.5);
+        }};
+        assertTrue(ProblemSet.average(list_dx) == 7.5);
 
-            ArrayList<Double> empty_list = new ArrayList<>();
+        ArrayList<Double> empty_list = new ArrayList<>();
         assertTrue(ProblemSet.average(empty_list) == 0.0);
-
-
-
-
-            System.out.println("" + ProblemSet.average(list));
-
-
-        System.out.println("" + ProblemSet.sumOfDigits(123));
+    }
+    @Test
+    public void sumOfDigitsTest() {
 
         assertTrue(ProblemSet.sumOfDigits(-36) == 9);
         assertTrue(ProblemSet.sumOfDigits(1234) == 10);
 
+        assertTrue(ProblemSet.sumOfDigits(57) == 12);
+        assertTrue(ProblemSet.sumOfDigits(-100) == 1);
 
+    }
 
-            HashMap<String, Integer> bills = new HashMap<>();
+    @Test
+    public void bestKeyTest()
+    {
+        HashMap<String, Integer> map1 = new HashMap<String, Integer>() {{
+            put("dog", 100);
+            put("cat", 99);
+            put("bird", 87);
+        }};
+        assertTrue(ProblemSet.bestKey(map1).equals("dog"));
 
-            bills.put("real", 100);
-            bills.put("real", 100);
+        map1 = new HashMap<String, Integer>() {{
+            put("dog", 100);
+            put("cat", 100);
+            put("bird", 87);
+        }};
+        assertTrue(ProblemSet.bestKey(map1).equals("dog") || ProblemSet.bestKey(map1).equals("cat"));
 
-            bills.put("real;y", 11);
+        map1 = new HashMap<String, Integer>() {{
 
+        }};
+        assertTrue(ProblemSet.bestKey(map1).equals(""));
 
-            System.out.print(ProblemSet.bestKey(bills));
-
-        }
-
-
+    }
 }
