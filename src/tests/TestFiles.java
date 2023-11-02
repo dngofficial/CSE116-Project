da4e83d9946a83d48edd617fcb95900782cc4d6a
Devan <dng8597@bths.edu>
2023-11-02 13:10:43 -0400

    continuing on task 6

--- src/tests/TestFiles.java
+++ src/tests/TestFiles.java
@@ -8,10 +8,15 @@ import ratings.datastructures.SongBayesianRatingComparator;
 import ratings.datastructures.SongTitleComparator;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 
 public class TestFiles {
@@ -29,67 +34,138 @@ public class TestFiles {
 
     }
 
-    public static boolean compareLists(ArrayList<String> list1, ArrayList<String> list2){
-        if (list1.size() != list2.size() || list1.isEmpty() || list2.isEmpty())
+    public static boolean compareLists(ArrayList<String> list1, ArrayList<String> list2) {
+        if(list1.size() == 0 || list2.size() == 0)
         {
-            return  false;
+            return false;
         }
-        for(int i = 0; i < list1.size(); i++){
-            if(!(list1.get(i).equals(list2.get(i)))){
-                return false;
+        else {
+            assertEquals(list1.size(), list2.size());
+            for (int i = 0; i < list1.size(); i++) {
+                System.out.println(list1.get(i));
+                System.out.println(list2.get(i));
+
+                assertEquals(list1.get(i), list2.get(i));
             }
+            return true;
         }
-        return true;
     }
 
-    public static boolean existenceMovie(ArrayList<Movie> list1, String title){
+    public static boolean existenceMovie(ArrayList<Movie> list1, String title) {
 
         assertNotNull(list1);
         assertTrue(!(list1.isEmpty()));
 
-        for(int i = 0; i < list1.size(); i++){
-            if(list1.get(i).getTitle().equals(title)){
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i).getTitle().equals(title)) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean testLists(LinkedListNode<Rating> node, ArrayList<Integer> list2)
-    {
-        if(node == null || (node.size() != list2.size())){
-            return false;
+    public void existenceMovie(ArrayList<Song> list1, Song song) {
+
+        assertNotNull(list1);
+        assertFalse(list1.isEmpty());
+
+        boolean bool = false;
+
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i).getTitle().equals(song.getTitle()) && list1.get(i).getSongID().equals(song.getSongID()) && list1.get(i).getArtist().equals(song.getArtist())) {
+                bool = true;
+            }
         }
-        else {
+        assertTrue(bool);
+    }
+
+
+    public static boolean testLists(LinkedListNode<Rating> node, ArrayList<Integer> list2) {
+        if (node == null) {
+            return false;
+        } else {
             int i = 0;
             LinkedListNode<Rating> temp = node;
             while (temp != null) {
-                if (temp.getValue().getRating() != list2.get(i)){ return false;}
+                if (temp.getValue().getRating() != list2.get(i)) {
+                    return false;
+                }
                 temp = temp.getNext();
                 i++;
             } //all values
         }
         return true;
-
     }
 
-    public void testMovieCast() {
-        ArrayList<String> zero_list = new ArrayList<>(){{add("Mary Steenburgen");add("Sean Patrick Flanery");add("Lance Henriksen");add("Jeff Goldblum");add("Brandon Smith");}};
+    public static void compareMovies(Movie movie1, Movie movie2) {
+        assertEquals(movie1.getTitle(), movie2.getTitle());
+        assertEquals(movie1.getCast(), movie2.getCast());
+    }
 
-        assertTrue(compareLists(zero_list, FileReader.readMovies("data/movies.csv").get(17).getCast()));
+    private void compareMoviesTrueTest(ArrayList<Movie> movie_list_1, ArrayList<Movie> movie_list_2){
+        assertEquals(movie_list_1.size(), movie_list_2.size());
+        for (int i = 0; i < movie_list_1.size(); i++) {
+            compareMovies(movie_list_1.get(i), movie_list_2.get(i));
+        }
+    }
+
+    public void testMovieInfo()
+    {
+        ArrayList<Movie> expected = new ArrayList<>(){{
+            add(new Movie("Powder", new ArrayList<String>() {{
+                add("Mary Steenburgen");
+                add("Sean Patrick Flanery");
+                add("Lance Henriksen");
+                add("Jeff Goldblum");
+                add("Brandon Smith");
+            }}));
+
+            add(new Movie("The Indian in the Cupboard", new ArrayList<String>() {{
+                add("Lindsay Crouse");
+                add("Richard Jenkins");
+                add("Steve Coogan");
+                add("David Keith");
+                add("Nestor Serrano");
+            }}));
+        }};
+
+        compareMoviesTrueTest(FileReader.readMovies("data/movie_test_1.csv"), expected);
+    }
+    @Test
+    public void testMovieCast() {
+        ArrayList<String> zero_list = new ArrayList<>() {{
+            add("Mary Steenburgen");
+            add("Sean Patrick Flanery");
+            add("Lance Henriksen");
+            add("Jeff Goldblum");
+            add("Brandon Smith");
+        }};
+
+        ArrayList<String> cast_list = FileReader.readMovies("data/movie_test_2.csv").get(0).getCast();
+        assertTrue(!(cast_list.isEmpty()));
+        if (!(cast_list.isEmpty())){
+            assertTrue(compareLists(cast_list, zero_list));
+        }
     }
 
     @Test
     public void testSongRatings() {
-        LinkedListNode<Rating> node = FileReader.readSongs("data/ratings.csv").get(1).getRatings();
-        ArrayList<Integer> zero_list = new ArrayList<>(){{add(5);add(4);add(4);}};
+        LinkedListNode<Rating> node = FileReader.readSongs("data/rating_test_1.csv").get(0).getRatings();
+        ArrayList<Integer> zero_list = new ArrayList<>() {{
+            add(5);
+            add(4);
+            add(4);
+        }};
 
+        System.out.println(zero_list);
         assertTrue(testLists(node, zero_list));
     }
+
     @Test
     public void testMovieEmpty() {
         assertTrue(FileReader.readMovies("data/moviesdne.csv").isEmpty());
     }
+
     @Test
     public void testSongEmpty() {
         assertTrue(FileReader.readSongs("data/ratingsdne.csv").isEmpty());
@@ -102,4 +178,59 @@ public class TestFiles {
 
 
 
+    public static boolean testMovOrder() {
+        try {
+            ArrayList<String> return_arraylist = new ArrayList<String>();
+            ArrayList<String> master_csv_lines = new ArrayList<>(Files.readAllLines(Paths.get("data/movies.csv")));
+            for (String unsplit_movie_list : master_csv_lines) {
+                ArrayList<String> split_string_movie = new ArrayList<>(Arrays.asList(unsplit_movie_list.split(",")));
+                return_arraylist.add(split_string_movie.get(0));
+            }
+
+            for (Movie mov : FileReader.readMovies("data/movies.csv")) {
+                if (!(return_arraylist.contains(mov.getTitle()))) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    @Test
+    public void testMovieListOrder() {
+        assertTrue(testMovOrder());
+    }
+
+    @Test
+    public void testSongBasicInfo1() {
+        existenceMovie(FileReader.readSongs("data/ratings.csv"), new Song("Purple Haze", "Jimmy Hendrix", "0wJoRiX5K5BxlqZTolB2LD"));
+    }
+@Test
+    public void testSongBasicInfoID() {
+        String id = FileReader.readSongs("data/rating_test_1.csv").get(0).getRatings().getValue().getReviewerID();
+
+        assertTrue(id.equals("264"));
+    }
+
+
+        public void compareLinkedLists(LinkedListNode<Rating> l1, LinkedListNode<Rating> l2) {
+        if(!(l1 == null && l2 == null))
+        {
+            assertTrue("l1 was null, but l2 was not", l1 != null);
+            assertTrue("l2 was null, but l1 was not", l2 != null);
+            assertTrue("node values are not equal",l1.getValue().getRating() == l2.getValue().getRating());
+            compareLinkedLists(l1.getNext(), l2.getNext());
+        }
+    }
+    @Test
+    public void testLinkedLists() {
+        LinkedListNode<Rating> expected = new LinkedListNode<>(new Rating("1", 4), null);
+        expected = new LinkedListNode<>(new Rating("1", 4), expected);
+        expected = new LinkedListNode<>(new Rating("1", 5), expected);
+
+        compareLinkedLists(expected, FileReader.readSongs("data/rating_test_1.csv").get(0).getRatings());
+    }
+
+}
